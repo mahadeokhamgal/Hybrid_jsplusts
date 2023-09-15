0eb985e796bfcf8ecb1baf11644b1e70fbe0c704
mkhamgal <mahadeokhamgal@gmail.com>
2023-09-16 04:55:46 +0530

    proxies changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { UpgradeModule } from '@angular/upgrade/static';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { APP_BASE_HREF } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     UpgradeModule
   ],
-  providers: [],
+  providers: [{provide: APP_BASE_HREF, useValue:'/'}],
   bootstrap: []
 })
 export class AppModule {
--- src/app/phone-list/phone-list.component.js
+++ src/app/phone-list/phone-list.component.js
@@ -4,7 +4,7 @@
 angular.
   module('phoneList').
   component('phoneList', {
-    templateUrl: 'http://localhost:7001/app/phone-list/phone-list.template.html',
+    templateUrl: './app/phone-list/phone-list.template.html',
     controller: ['Phone',
       function PhoneListController(Phone) {
         this.phones = Phone.query();
