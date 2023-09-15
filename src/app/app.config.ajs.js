c7cd677df7ef3425fc26b341b6ed780afb7d2299
mkhamgal <mahadeokhamgal@gmail.com>
2023-09-16 04:21:22 +0530

    Initial commit as child

--- src/app/app.config.ajs.js
+++ src/app/app.config.ajs.js
@@ -1,5 +1,5 @@
 export default function(appModule) {
-    appModule.config(function ($routeProvider) {
+    appModule.config(function ($routeProvider, $sceDelegateProvider) {
         $routeProvider.
             when('/phones', {
             template: '<phone-list></phone-list>'
@@ -8,5 +8,13 @@ export default function(appModule) {
             template: '<phone-detail></phone-detail>'
             }).
             otherwise('/phones');
+        $sceDelegateProvider
+            .resourceUrlWhitelist([
+            // Allow same origin resource loads.
+            'self','**',
+            // Allow loading from our assets domain. **.
+            'http://localhost:9000/**',
+            'http://localhost:7001/**'
+            ]);
     });
 }
